a758240bbd3adffbffd1d602b509c42769abfd1c
unknown <sohrawardy1998@gmail.com>
2024-08-25 20:46:41 +0600

    post donor data on database

--- index.js
+++ index.js
@@ -23,16 +23,26 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const coffeeCollection = client.db("FinalAssignment").collection("donor");
+
+    //sending on server
+    app.post("/donor", async (req, res) => {
+      const newDonor = req.body;
+      console.log(newDonor);
+      const result = await coffeeCollection.insertOne(newDonor);
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    //await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
