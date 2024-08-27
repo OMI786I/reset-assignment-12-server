00183f70b5d80b9e08627e93bc996620d06910a6
unknown <sohrawardy1998@gmail.com>
2024-08-28 01:16:46 +0600

    delete api for donor request

--- index.js
+++ index.js
@@ -146,6 +146,15 @@ async function run() {
       console.log(result);
     });
 
+    //for delete
+
+    app.delete("/requestDonor/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await donorRequestCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
     //await client.connect();
     // Send a ping to confirm a successful connection
